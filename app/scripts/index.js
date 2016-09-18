32a76e946eeb275d3dfcd05dcf478c427dfc0845
Matthew Smiglarski <matthew.smiglarski@simulity.com>
2016-09-18 18:44:35 +0100

    Use the scripts directory.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GrammarGuide from './grammar';
-import initApp from '../../app.js';
+import initApp from './app.js';
 require('../../style.css');
 
 ReactDOM.render(
