40cd86136665367634ba6341d383642e0084984f
Matthew Smiglarski <matthew.smiglarski@simulity.com>
2016-09-18 18:47:51 +0100

    Use SASS

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GrammarGuide from './grammar';
 import initApp from './app.js';
-require('../../style.css');
+require('../styles/style.scss');
 
 ReactDOM.render(
     <div>
